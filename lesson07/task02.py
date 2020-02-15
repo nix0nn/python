5b867f37b0273c8e4e471e0bd5c6a23b4bdb6a7f
EvgeniyGolodov <nixnn0323@gmail.com>
2020-02-15 17:51:01 +0500

    Задание №2

---
+++ lesson07/task02.py
@@ -0,0 +1,38 @@
+from abc import abstractmethod
+
+
+class Clothes:
+    def __init__(self, name):
+        self.name = name
+    @abstractmethod
+    def consumption(self):
+        pass
+
+
+class Coat(Clothes):
+    def __init__(self, name, v):
+        super().__init__(name)
+        self._v = v
+
+    @property
+    def consumption(self):
+        v = self._v / 6.5 + 0.5
+        return v
+
+
+class Suit(Clothes):
+    def __init__(self, name, h):
+        super().__init__(name)
+        self._h = h
+
+    @property
+    def consumption(self):
+        h = 2 * self._h + 0.3
+        return h
+
+
+a = Coat('пальто', 48)
+b = Suit('костюм', 1.75)
+
+print(a.consumption + b.consumption)
+
